c081d20a5f49f61ec3ccd0751facd07aa5c06edc
spe11001 <56440075+spe11001@users.noreply.github.com>
2021-02-21 14:53:51 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ReactDemo.Models;
+
+namespace ReactDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/tutorial.jsx
@@ -0,0 +1,40 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
+class CommentList extends React.Component {
+    render() {
+        return (
+            <div className="commentList">Hello, world! I am a CommentList.</div>
+        );
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            <div className="commentForm">Hello, world! I am a CommentForm.</div>
+        );
+    }
+}
+
+class Comment extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
